a764c421b78bab1a713ddf1f21334e2467072242
mbdrew <mdrew@WIN7-5.org.lrwu.com>
2015-02-19 16:07:44 -0600

    2/19/15 -- 4:00PM

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-This the the main.js file
+require(["application/bootstrapmap",
+         "esri/layers/ArcGISDynamicMapServiceLayer",
+         "esri/layers/ArcGISTiledMapServiceLayer",
+         "esri/geometry/Extent",
+         "esri/SpatialReference",
+         "esri/dijit/BasemapLayer",
+         "esri/dijit/Basemap",
+         "esri/dijit/BasemapGallery",
+         "dojo/domReady!"], 
+  function(BootstrapMap, 
+          ArcGISDynamicMapServiceLayer, 
+          ArcGISTiledMapServiceLayer, 
+          Extent, 
+          SpatialReference, 
+          BasemapLayer, 
+          Basemap, 
+          BasemapGallery) {
+
+    // Get a reference to the ArcGIS Map class
+    var initExtent = new Extent({ "xmin": 1170000, "ymin": 90000, "xmax": 1263200, "ymax": 181350, "spatialReference": { "wkid": 3433 } });
+    var map = BootstrapMap.create("mapDiv",{
+
+      //center:[1200000,140000],
+      //zoom:12,
+      //scrollWheelZoom: false
+      extent: initExtent});
+
+    dynLayer = new ArcGISDynamicMapServiceLayer("http://maps.lrwu.com/wa/rest/services/MoreLayers/MapServer");
+    tiledLayer = new ArcGISDynamicMapServiceLayer("http://www.pagis.org/ArcGIS/rest/services/MAPS/BaseMap/MapServer");
+
+    console.log("next line is map.addLayer");
+    map.addLayers([tiledLayer, dynLayer]);
+});
