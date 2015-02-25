b9a3fa57375d0667e517304e569987d7d02bc5af
mbdrew <mdrew@WIN7-5.org.lrwu.com>
2015-02-25 13:52:02 -0600

    "commited some changes"

--- js/main.js
+++ js/main.js
@@ -6,28 +6,55 @@ require(["application/bootstrapmap",
          "esri/dijit/BasemapLayer",
          "esri/dijit/Basemap",
          "esri/dijit/BasemapGallery",
+         "agsjs/dijit/TOC",
          "dojo/domReady!"], 
   function(BootstrapMap, 
-          ArcGISDynamicMapServiceLayer, 
-          ArcGISTiledMapServiceLayer, 
-          Extent, 
-          SpatialReference, 
-          BasemapLayer, 
-          Basemap, 
-          BasemapGallery) {
+    ArcGISDynamicMapServiceLayer, 
+    ArcGISTiledMapServiceLayer, 
+    Extent, 
+    SpatialReference, 
+    BasemapLayer, 
+    Basemap, 
+    BasemapGallery,
+    TOC)
+    {
 
-    // Get a reference to the ArcGIS Map class
-    var initExtent = new Extent({ "xmin": 1170000, "ymin": 90000, "xmax": 1263200, "ymax": 181350, "spatialReference": { "wkid": 3433 } });
-    var map = BootstrapMap.create("mapDiv",{
+      // Get a reference to the ArcGIS Map class
+      var initExtent = new Extent({ "xmin": 1170000, "ymin": 90000, "xmax": 1263200, "ymax": 181350, "spatialReference": { "wkid": 3433 } });
+      var map = BootstrapMap.create("mapDiv",{
 
-      //center:[1200000,140000],
-      //zoom:12,
-      //scrollWheelZoom: false
-      extent: initExtent});
+        //center:[1200000,140000],
+        //zoom:12,
+        //scrollWheelZoom: false
+        extent: initExtent});
 
-    dynLayer = new ArcGISDynamicMapServiceLayer("http://maps.lrwu.com/wa/rest/services/MoreLayers/MapServer");
-    tiledLayer = new ArcGISDynamicMapServiceLayer("http://www.pagis.org/ArcGIS/rest/services/MAPS/BaseMap/MapServer");
+      dynLayer = new ArcGISDynamicMapServiceLayer("http://maps.lrwu.com/wa/rest/services/MoreLayers/MapServer");
+      tiledLayer = new ArcGISDynamicMapServiceLayer("http://www.pagis.org/ArcGIS/rest/services/MAPS/BaseMap/MapServer");
 
-    console.log("next line is map.addLayer");
-    map.addLayers([tiledLayer, dynLayer]);
-});
+
+
+            // Code for displaying TOC
+      map.on('layers-add-result', function (evt) {
+        // overwrite the default visibility of service.
+        // TOC will honor the overwritten value.
+        dynLayer.setVisibleLayers([0,1]);
+        toc = new TOC({
+          map: map,
+          layerInfos: [{
+            layer: dynLayer,
+            title: "Layers"
+            //collapsed: false, // whether this root layer should be collapsed initially, default false.
+            //slider: false // whether to display a transparency slider.
+          }]
+        }, 'tocDiv');
+        toc.startup();
+        toc.on('load', function () {
+          if (console)
+            console.log('TOC loaded');
+        });
+      });
+
+      console.log("next line is map.addLayer");
+      map.addLayers([tiledLayer, dynLayer]);
+
+    });
