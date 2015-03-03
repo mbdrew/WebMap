4945f0a0de81edf4cf6f61fc3bc3ccd34f6b8035
mbdrew <mdrew@WIN7-5.org.lrwu.com>
2015-03-03 14:30:42 -0600

    added search tools (preliminary)

--- js/main.js
+++ js/main.js
@@ -6,7 +6,15 @@ require(["application/bootstrapmap",
          "esri/dijit/BasemapLayer",
          "esri/dijit/Basemap",
          "esri/dijit/BasemapGallery",
+         "esri/dijit/Geocoder",
+         "esri/tasks/IdentifyParameters",
+         "esri/tasks/IdentifyTask",
          "agsjs/dijit/TOC",
+         "esri/graphic",
+         "esri/symbols/SimpleFillSymbol",
+         "esri/symbols/SimpleMarkerSymbol",
+         "esri/symbols/SimpleLineSymbol",
+         "dojo/_base/Color",         
          "dojo/dom",
          "dojo/dom-style",
          "dojo/dom-class",
@@ -20,7 +28,15 @@ require(["application/bootstrapmap",
     BasemapLayer, 
     Basemap, 
     BasemapGallery,
+    Geocoder,
+    IdentifyParameters,
+    IdentifyTask,
     TOC,
+    Graphic,
+    SimpleFillSymbol,
+    SimpleMarkerSymbol,
+    SimpleLineSymbol,
+    Color,
     dom,
     domStyle,
     domClass,
@@ -69,6 +85,130 @@ require(["application/bootstrapmap",
 
       console.log("next line is map.addLayer");
       map.addLayers([tiledLayer, dynLayer]);
+
+      identifyTask = new IdentifyTask("http://maps.lrwu.com/wa/rest/services/MoreLayers/MapServer");
+
+      // Add geocoder for addresses
+
+      var gc;
+      gc = [{
+        url: "http://maps.lrwu.com/wa/rest/services/BPADD_SingleField/GeocodeServer",
+        name: "BPADD_SingleField",
+        singleLineFieldName: "Single Line Input",
+        placeholder: "Enter Address"
+      }];
+
+      geocoder = new Geocoder({
+        map: map,
+        autoComplete: true,
+        autoNavigate: true,
+        zoomScale: 3000,
+        geocoders: gc,
+        arcgisGeocoder: false
+      }, "addrsearch");
+      geocoder.startup();
+      var handle = on(geocoder, "select", zoomTo);    
+
+
+      // Add Geocoder for manholes
+
+      var gcMH;
+      gcMH = [{
+        url: "http://maps.lrwu.com/wa/rest/services/Manholes_SingleField/GeocodeServer",
+        name: "Manholes_SingleField",
+        singleLineFieldName: "Single Line Input",
+        placeholder: "Enter Manhole"
+      }];
+
+      geocoderMH = new Geocoder({
+        map: map,
+        autoComplete: true,
+        autoNavigate: true,
+        zoomScale: 1000,
+        geocoders: gcMH,
+        arcgisGeocoder: false
+      }, "manholesearch");
+      geocoderMH.startup();
+      var handleMH = on(geocoderMH, "select", zoomTo);
+
+
+      // Add Geocoder for mapsheets
+
+      var gcMS;
+      gcMS = [{
+        url: "http://maps.lrwu.com/wa/rest/services/Mapsheets_SingleField/GeocodeServer",
+        name: "Mapsheets_SingleField",
+        singleLineFieldName: "Single Line Input",
+        placeholder: "Enter Mapsheet"
+      }];
+
+      geocoderMS = new Geocoder({
+        map: map,
+        autoComplete: true,
+        autoNavigate: false,
+        zoomScale: 1000,
+        geocoders: gcMS,
+        arcgisGeocoder: false
+      }, "mapsheetsearch");
+      geocoderMS.startup();
+      var handleMS = on(geocoderMS, "select", getMSShape);
+
+
+      function getMSShape(evt) {
+        // Gets the mapssheet shape
+        console.log("The return value is: ");
+        console.log(evt.result.feature.geometry);
+
+        identifyParams = new IdentifyParameters();
+        identifyParams.tolerance = 1;
+        identifyParams.returnGeometry = true;
+        identifyParams.layerIds = [6];
+        identifyParams.layerOption = IdentifyParameters.LAYER_OPTION_ALL;
+        identifyParams.width = map.width;
+        identifyParams.height = map.height;
+        identifyParams.mapExtent = map.extent;
+        identifyParams.geometry = evt.result.feature.geometry;
+        identifyTask.execute(identifyParams, function (results) { zoomToMS(results); });
+      }
+
+      function zoomToMS(result) {
+        // Put an outline of the mapsheet on the map and zoom to the user-selected mapsheet.  
+        console.log("results from zoomToMS [0]");
+        console.log(result[0].feature.geometry);
+
+        var lineSymbol = new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([0, 0, 255]), 3);
+        var polySymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_NULL, lineSymbol, new Color([0, 0, 0]));
+        var graphic = new Graphic(result[0].feature.geometry, polySymbol);
+        map.graphics.add(graphic);
+        //console.log("Extent: " + result[0].feature.geometry.getExtent)
+        var msExtent = result[0].feature.geometry.getExtent().expand(1.25);
+        map.setExtent(msExtent);
+
+        //closeSearchTool();
+      }  // End zoomToMs function
+
+
+      function zoomTo(evt) {
+        console.log("hello from zoomTo...");
+
+        //map.graphics.clear();
+
+        // Puts graphic on map to show where address is.
+        var symbol = new esri.symbol.PictureMarkerSymbol({
+          "angle": 0,
+          "xoffset": 0,
+          "yoffset": 10,
+          "type": "esriPMS",
+          "url": "images/GreenPin1LargeB.png",
+          "contentType": "image/png",
+          "width": 24,
+          "height": 24
+        });
+        var graphic = new Graphic(evt.result.feature.geometry, symbol);
+        map.graphics.add(graphic);
+
+        //closeSearchTool();
+      }  // End zoomTo function  
 
     });
 
